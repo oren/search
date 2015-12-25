1ed43ac98b5d7c0896fcba91f78c6097b0a9a4d4
Oren <orengolan@gmail.com>
2015-12-24 21:48:12 -0800

    misc

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"log"
 )
 
+// slice of maps. each map hase k/v that both are strings
 type mytype []map[string]string
 
 func main() {
+
 	var data mytype
 	file, err := ioutil.ReadFile("products-small.json")
 	if err != nil {
@@ -19,5 +21,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Println(data)
 }
