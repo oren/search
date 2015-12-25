f2c902143d5a1167980f57f64e11f4d64a503b8d
Oren <orengolan@gmail.com>
2015-12-24 22:53:32 -0800

    misc

--- main.go
+++ main.go
@@ -1,26 +1,55 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 )
 
 // slice of maps. each map hase k/v that both are strings
-type mytype []map[string]string
+// type mytype []map[string]string
+
+type product struct {
+	ID    string `json:id`
+	Title string `json:title`
+	Price string `json:price`
+}
 
 func main() {
+	// map of string -> product
+	products := make(map[string]product)
+	products["1"] = product{ID: "1", Title: "usb 3.0 8GB", Price: "5.99"}
+	products["2"] = product{ID: "2", Title: "usb 3.0 4GB", Price: "3.99"}
 
-	var data mytype
-	file, err := ioutil.ReadFile("products-small.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = json.Unmarshal(file, &data)
-	if err != nil {
-		log.Fatal(err)
+	// map of string -> [int, int, int]
+	keywords := make(map[string][]int)
+	keywords["usb"] = []int{1, 2}
+	keywords["3.0"] = []int{1, 2}
+	keywords["8GB"] = []int{1}
+	keywords["4GB"] = []int{2}
+
+	// slice of strings
+	searchTerm := []string{"usb", "4GB", "foo"}
+	results := search(searchTerm, products, keywords)
+	fmt.Println(results)
+}
+
+func search(searchTerm []string, products map[string]product, keywords map[string][]int) []product {
+	score := make(map[int]int)
+	// for each search term
+	// find its slice
+	// for each number in the slice, increment a scoring map
+	for _, term := range searchTerm {
+		fmt.Println(keywords[term])
+		for _, productNumber := range keywords[term] {
+			fmt.Println(productNumber)
+			score[productNumber] += 1
+		}
 	}
 
-	fmt.Println(data)
+	score2 := make(PairList, len(score))
+	score2 = RankByWordCount(score)
+	fmt.Println(score2)
+	// return the top 5 products
+	results := []product{products["1"], products["2"]}
+
+	return results
 }
---
+++ sort.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"sort"
+)
+
+func RankByWordCount(wordFrequencies map[int]int) PairList {
+	pl := make(PairList, len(wordFrequencies))
+	i := 0
+	for k, v := range wordFrequencies {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(sort.Reverse(pl))
+	return pl
+}
+
+type Pair struct {
+	Key   int
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
